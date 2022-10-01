723989ce0feb07e1c7f5c5a7210e3e60a9d41a18
Angelo Bennett <77983302+ahbenn86@users.noreply.github.com>
2022-10-01 00:21:19 -0400

    silhouette body, buttons and chakras

---
+++ readme.md
@@ -0,0 +1,18 @@
+# App name
+### App description.  
+<br/>
+
+![Insert Image Link Here]()
+
+> Currently under development 
+
+<br/>
+
+#### Languages and tools 
+---
+
+* HTML
+* CSS
+* Javascript
+* Node
+* React (JSX)
---
+++ script.js
@@ -0,0 +1,59 @@
+const rootBtn = document.querySelector("#root-btn");
+const rootText = document.querySelector(".root-chakra-text");
+const rootChakra = document.querySelector(".red-chakra");
+const sacralBtn = document.querySelector("#sacral-btn");
+const sacralText = document.querySelector(".sacral-chakra-text");
+const sacralChakra = document.querySelector(".orange-chakra");
+const solarBtn = document.querySelector("#solar-btn");
+const solarText = document.querySelector(".solar-chakra-text");
+const solarChakra = document.querySelector(".yellow-chakra");
+const heartBtn = document.querySelector("#heart-btn");
+const heartText = document.querySelector(".heart-chakra-text");
+const heartChakra = document.querySelector(".green-chakra");
+const throatBtn = document.querySelector("#throat-btn");
+const throatText = document.querySelector(".throat-chakra-text");
+const throatChakra = document.querySelector(".light-blue-chakra");
+const thirdEyeBtn = document.querySelector("#third-eye-btn");
+const thirdEyeText = document.querySelector(".third-eye-chakra-text");
+const thirdEyeChakra = document.querySelector(".blue-chakra");
+const crownBtn = document.querySelector("#crown-btn");
+const crownText = document.querySelector(".crown-chakra-text");
+const crownChakra = document.querySelector(".purple-chakra");
+
+rootBtn.addEventListener("click", () => {
+    rootText.style = "visibility: visible;"
+    rootChakra.style = "animation: animate 3s infinite;"
+    
+});
+
+sacralBtn.addEventListener("click", () => {
+    sacralText.style = "visibility: visible;"
+    sacralChakra.style = "animation: animate 3s infinite;"
+    
+});
+
+solarBtn.addEventListener("click", () => {
+    solarText.style = "visibility: visible;"
+    solarChakra.style = "animation: animate 3s infinite;"
+    
+});
+heartBtn.addEventListener("click", () => {
+    heartText.style = "visibility: visible;"
+    heartChakra.style = "animation: animate 3s infinite;"
+    
+});
+throatBtn.addEventListener("click", () => {
+    throatText.style = "visibility: visible;"
+    throatChakra.style = "animation: animate 3s infinite;"
+    
+});
+thirdEyeBtn.addEventListener("click", () => {
+    thirdEyeText.style = "visibility: visible;"
+    thirdEyeChakra.style = "animation: animate 3s infinite;"
+    
+});
+crownBtn.addEventListener("click", () => {
+    crownText.style = "visibility: visible;"
+    crownChakra.style = "animation: animate 3s infinite;"
+    
+});
